21e211c39877ed5598768289733838f9e966d24a
xoghd0729 <132333838+xoghd0729@users.noreply.github.com>
2026-05-10 23:50:55 +0900

    Add app.js

---
+++ app.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+const dotenv = require('dotenv');
+
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const nunjucks = require('nunjucks');
+const { sequelize } = require('./models');
+
+const userRouter = require('./routes/user');
+const commentRouter = require('./routes/comment');
+const indexRouter = require('./routes');
+
+
+dotenv.config();
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.set('view engine', 'html');
+nunjucks.configure(path.join(__dirname, 'views'), {
+    express: app,
+    watch: true,
+});
+
+sequelize.sync({ force: false })
+  .then(() => console.log('데이터베이스 연결 성공'))
+  .catch(err => console.error(err));
+
+app.use(
+    morgan('dev'),
+    express.static(path.join(__dirname, 'public')),
+    express.json(),
+    express.urlencoded({ extended: false }),
+    cookieParser(process.env.SECRET),
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false
+        },
+        name: 'session-cookie'
+    })
+);
+
+app.use('/user', userRouter);
+app.use('/comment', commentRouter);
+app.use('/', indexRouter);
+
+app.use((req, res) =>
+    res.render('index', {
+        title: require('./package.json').name,
+        port: app.get('port')
+    }));
+    
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err);
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
